6ff0b7b5d5d08b2a91c93e8479daa2b19bdd9df2
tameooo <72303445+tameoo@users.noreply.github.com>
2021-05-03 15:09:30 +0600

    Update Main.jsx

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -329,25 +329,6 @@ const Main = () => {
                     </div>
                 </section>
             </main>
-             {/* <svg aria-hidden="true" width="100%" height="100%" viewBox="0 0 70 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <mask id="u399-star-rating-mask--34" data-purpose="star-rating-mask">
-                                            <rect x="0" y="0" width="90%" height="100%" fill="white"></rect>
-                                        </mask>
-                                        <g class="star-rating--star-filled--3D4Ee" mask="url(#u399-star-rating-mask--34)" data-purpose="star-filled">
-                                            <use xlinkHref="#icon-rating-star" width="14" height="14" x="0"></use>
-                                            <use xlinkHref="#icon-rating-star" width="14" height="14" x="14"></use>
-                                            <use xlinkHref="#icon-rating-star" width="14" height="14" x="28"></use>
-                                            <use xlinkHref="#icon-rating-star" width="14" height="14" x="42"></use>
-                                            <use xlinkHref="#icon-rating-star" width="14" height="14" x="56"></use>
-                                        </g>
-                                        <g fill="transparent" class="star-rating--star-bordered--2QFon" stroke-width="2" data-purpose="star-bordered">
-                                            <use xlinkHref="#icon-rating-star" width="12" height="12" x="1" y="1"></use>
-                                            <use xlinkHref="#icon-rating-star" width="12" height="12" x="15" y="1"></use>
-                                            <use xlinkHref="#icon-rating-star" width="12" height="12" x="29" y="1"></use>
-                                            <use xlinkHref="#icon-rating-star" width="12" height="12" x="43" y="1"></use>
-                                            <use xlinkHref="#icon-rating-star" width="12" height="12" x="57" y="1"></use>
-                                        </g>
-                                    </svg> */}
         </div>
     )
 }
